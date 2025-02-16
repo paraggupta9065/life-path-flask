9f50c2837f89a23ab5fd0ee089f9897637522aa2
parag gupta <paraggupta@parags-MacBook-Air.local>
2025-02-17 01:27:50 +0530

    image url update

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-AWS_ACCESS_KEY_ID = "AKIA6JKEYBDDJKUIYVAP"
-AWS_SECRET_ACCESS_KEY = "ej7khAshi3bYWVBdDLdN/e/GJtSn0+/zKZnauNHb"
-AWS_S3_BUCKET_NAME = "lifepath"
-AWS_S3_REGION = "eu-north-1"  # Example: us-east-1
